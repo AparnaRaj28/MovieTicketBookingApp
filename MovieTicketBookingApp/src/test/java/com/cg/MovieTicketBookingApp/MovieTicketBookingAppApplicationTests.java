fa93a72d7534e22be57d1b50a81a1b01b20636cf
Aparna Raj <aparnaraj448@gmail.com>
2022-04-21 22:30:38 +0530

    first commit

---
+++ MovieTicketBookingApp/src/test/java/com/cg/MovieTicketBookingApp/MovieTicketBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.MovieTicketBookingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
